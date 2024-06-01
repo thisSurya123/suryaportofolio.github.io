c639ccbc885a081df444a9abe59bb329ffab17c2
thissurya123 <thissurya31@gmail.com>
2024-06-01 14:30:43 +0700

    commite change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-console.log('Js connected');
-const img_platform = document.getElementById('img-platform');
-const user_platform = document.getElementById('user-platform');
+// console.log('Js connected');
+// const img_platform = document.getElementById('img-platform');
+// const user_platform = document.getElementById('user-platform');
 
-img_platform.addEventListener("mouseover", function(){
-    user_platform.style.display = "inline-block";
-});
+// img_platform.addEventListener("mouseover", function(){
+//     user_platform.style.display = "inline-block";
+// });
